996ffe9d2a9c1709689cf2e444027ac9cee11371
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-04-01 11:25:04 -0600

    add lessons

--- config.md
+++ config.md
@@ -1,14 +1,15 @@
 Babel used to transpile from JSX to JS
-ReactDOM.render(JSX, document.getElementById('root')) placed inside JSX
 JS always in { 'this is treated as JavaScript code' }
 JSX must return a single element
 ex: const JSX = <h1>Hello JSX!</h1>;
 
 <!-- {/* */} << this only --> for comments
 
-ReactDOM.render(componentToRender, targetNode) to render React elements to DOM
-camelCase for everything.
-class = className
-<br> = <br />
+1. ReactDOM.render(componentToRender, targetNode) to render React elements to DOM
+   camelCase for everything.
+   (ex. ReactDOM.render(JSX, document.getElementById('root')) placed inside JSX)
+2. ReactDOM.render(<ComponentToRender />, targetNode)
+   class = className
+   <br> = <br />
 
 <hr> = <hr />
--- index.jsx
+++ index.jsx
@@ -1,4 +1,5 @@
 //render the ChildComponent in the ParentComponent
+/*React component is an ES6 class which extends React.Component */
 const ChildComponent = () => {
   return (
     <div>
@@ -56,6 +57,65 @@ class TypesOfFood extends React.Component {
       <div>
         <h1>Types of Food:</h1>
         <Fruits />
+      </div>
+    );
+  }
+}
+//(Render a Class Component to the DOM)
+//ex.:ReactDOM.render(<ComponentToRender />, targetNode);
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        <Fruits />
+        <Vegetables />
+      </div>
+    );
+  }
+}
+ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'));
+
+//React Component from Scratch:
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<MyComponent />, getElementById('challenge-node'));
+
+//Passing Props to Class Component:
+const CurrentDate = (props) => {
+  return (
+    <div>
+      {/* HERE */}
+      <p>The current date is: {props.date}</p>
+      {/* HERE */}
+    </div>
+  );
+};
+
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>What date is it?</h3>
+        {/* HERE */}
+        <CurrentDate date={Date()} />
+        {/* HERE */}
       </div>
     );
   }
