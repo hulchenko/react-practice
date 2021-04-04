e8e336ea01fa906820e07140f6abc41ee5100bb1
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-04-03 20:10:44 -0600

    add solution

--- index.jsx
+++ index.jsx
@@ -362,3 +362,41 @@ class Counter extends React.Component {
     );
   }
 }
+
+//Controlled input/form:
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value,
+    });
+  }
+  handleSubmit(event) {
+    // HERE
+    event.preventDefault();
+    this.setState({
+      submit: this.state.input,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          {/* HERE */}
+          <input value={this.state.input} onChange={this.handleChange}></input>
+          <button type="submit">Submit!</button>
+        </form>
+        {/* HERE */}
+        <h1>{this.state.submit}</h1>
+      </div>
+    );
+  }
+}
