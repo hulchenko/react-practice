dfd0f94c177030c10657e82268786d05220f4deb
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-04-09 12:09:42 -0600

    combine solutions

--- redux-practice.jsx
+++ redux-practice.jsx
@@ -1,112 +1,75 @@
-//Reducer only task: a pure function that takes state and action, then returns new state.
-//#1 Declare store:
-const reducer = (state = 5) => {
-  return state;
-};
-const store = Redux.createStore(reducer);
-//#2 Get state/data from it:
-const currentState = store.getState();
-//#3 Defining Redux action:
-const action = {
-  type: 'LOGIN',
-};
-//#4 Dispatch action:
-const store = Redux.createStore((state = { login: false }) => state);
-
-const loginAction = () => {
-  return {
-    type: 'LOGIN',
-  };
-};
-// HERE
-store.dispatch(loginAction());
-
-//Combining multiple reducers:
-const INCREMENT = 'INCREMENT';
-const DECREMENT = 'DECREMENT';
-
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case INCREMENT:
-      return state + 1;
-    case DECREMENT:
-      return state - 1;
-    default:
-      return state;
-  }
-};
-
-const LOGIN = 'LOGIN';
-const LOGOUT = 'LOGOUT';
-
-const authReducer = (state = { authenticated: false }, action) => {
-  switch (action.type) {
-    case LOGIN:
-      return {
-        authenticated: true,
-      };
-    case LOGOUT:
-      return {
-        authenticated: false,
-      };
-    default:
-      return state;
-  }
-};
-
-const rootReducer = Redux.combineReducers({
-  count: counterReducer,
-  auth: authReducer,
-});
-
-const store = Redux.createStore(rootReducer);
-
-//Object assign:
-const defaultState = {
-  user: 'CamperBot',
-  status: 'offline',
-  friends: '732,982',
-  community: 'freeCodeCamp',
-};
-
-const immutableReducer = (state = defaultState, action) => {
-  switch (action.type) {
-    case 'ONLINE':
-      return Object.assign({}, state, { status: 'online' });
-    default:
-      return state;
-  }
-};
-
-const wakeUp = () => {
-  return {
-    type: 'ONLINE',
-  };
-};
-
-const store = Redux.createStore(immutableReducer);
-
-// ====================================
-
-const ADD = 'ADD'; //action type ADD
+// Redux:
+const ADD = 'ADD';
 
 const addMessage = (message) => {
-  //action creator addMessage()
   return {
     type: ADD,
-    message: message,
+    message,
   };
 };
 
 const messageReducer = (state = [], action) => {
-  //reducer
   switch (action.type) {
     case ADD:
       return [...state, action.message];
-      break;
     default:
       return state;
   }
 };
 
-const store = Redux.createStore(messageReducer); // Redux store
+const store = Redux.createStore(messageReducer);
+
+// React:
+
+class DisplayMessages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      messages: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value,
+    });
+  }
+  submitMessage() {
+    this.setState((state) => {
+      const currentMessage = state.input;
+      return {
+        input: '',
+        messages: state.messages.concat(currentMessage),
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        <input value={this.state.input} onChange={this.handleChange} />
+        <br />
+        <button onClick={this.submitMessage}>Submit</button>
+        <ul>
+          {this.state.messages.map((message, idx) => {
+            return <li key={idx}>{message}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const Provider = ReactRedux.Provider;
+
+class AppWrapper extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <DisplayMessages />
+      </Provider>
+    );
+  }
+}
